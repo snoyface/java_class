12a9c9b2f394379372ad190fb017213622f13af7
snoyface <jmacleod@g.harvard.edu>
2014-10-15 10:29:22 -0400

    test cases
    -worked with unit 22 prob 4 (messed up class again)
    -updated answers on unit 22 Part B.txt
    -deleted Charity (prob 5) off Partb.txt

---
+++ test_cases/Unit22_Prob4.java
@@ -0,0 +1,12 @@
+class Unit22_Prob4{
+	public static void main (String [] args){
+		int n = 16;
+		int m = 18;
+		double x = 2.5;
+
+		System.out.println ( m / n + m % n);
+		System.out.println (5 * x - n / 5);
+		System.out.println ( Math.sqrt (Math.sqrt (n)));
+		System.out.println ( 1 - (1 - (1 - n) * x));
+	}
+}
---
+++ unit_22/Prob4.java
@@ -0,0 +1,12 @@
+class Prob4{
+	public static void main (String [] args){
+		int n = 16;
+		int m = 18;
+		double x = 2.5;
+
+		System.out.println ("A is: " m / n + m % n);
+		System.out.println ("B is: " 5 * x - n / 5);
+		System.out.println ("C is: " Math.sqrt (Math.sqrt (n)));
+		System.out.println ("D is: " 1 - (1 - (1 - n) * x)):
+	}
+}
