2a555ce40b9e6ea67fe667943b1e0115b4674892
snoyface <jmacleod@g.harvard.edu>
2014-10-16 13:29:37 -0400

    created increase.java
    new problem to work on.

---
+++ unit_22/Increase.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Increase{
+	public static void main (String [] args){
+
+		Scanner keyboard = new Scanner (System.in);
+/*
+		int[] day;
+		day = new int[10];
+
+		for(int i = 1; i <= 10; i++){
+			System.out.println("Enter day" + i + "amount");
+			int days = keyboard.nextInt;
+			days = day[];
+
+		}
+		System.out.println(day);
+*/
+		System.out.println("Enter day 1 amount")
+		int day1 = keyboard.nextInt();
+		System.out.println("Enter day 2 amount")
+		int day2 = keyboard.nextInt();
+		System.out.println("Enter day 3 amount")
+		int day3 = keyboard.nextInt();
+		System.out.println("Enter day 4 amount")
+		int day4 = keyboard.nextInt();
+		System.out.println("Enter day 5 amount")
+		int day5 = keyboard.nextInt();
+		System.out.println("Enter day 6 amount")
+		int day6 = keyboard.nextInt();
+		System.out.println("Enter day 7 amount")
+		int day7 = keyboard.nextInt();
+		System.out.println("Enter day 8 amount")
+		int day8 = keyboard.nextInt();
+		System.out.println("Enter day 9 amount")
+		int day9 = keyboard.nextInt();
+		System.out.println("Enter day 10 amount")
+		int day10 = keyboard.nextInt();
+
+		
+
+
+	}
+}
