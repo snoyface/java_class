b617a5dff8c17ebf79863d5121bf85712bff34f5
snoyface <jmacleod@g.harvard.edu>
2014-11-01 17:42:45 -0400

    comments, ints, start of if statement
    small update

--- unit_31/Craps.java
+++ unit_31/Craps.java
@@ -1,7 +1,10 @@
 /*
-* prints the rolls to a Craps game
+* Prints the rolls to a Craps game, rolls till the number is 4,5,6,7,8,9,10. 
+* that is the players "point". Then continues to roll till either they 
+* roll a 7 (lose), or they roll their "point" (win).
 * @author Jonathan MacLeod
 * @version Oct 31, 2014
+* 
 */
 
 import java.math.*;
@@ -10,10 +13,13 @@ class Craps
 {
 	public static void main (String [] args)
 	{
-		int one = rint(1,2);
-		int two = rint(3,4);
-
+		int one =   rint(1,6);   //first die
+		int two =   rint(1,6);   //second die
+		int total;               //total of both dice added together
+		int point; 				 //the players point
+		//if()
 		System.out.print ("Computer rolls a " + one + " and a " + two);
+
 		System.out.print (" , for a total of " + (one + two) + "\n");
 	}
 
