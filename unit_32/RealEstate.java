8dfdb69db3f246fe214ba7e9283aa58470b28b38
snoyface <jmacleod@g.harvard.edu>
2014-11-30 22:18:39 -0500

    Fix RealEstate
    -uses lineFixer method to cut out vowels
    -uses notVowel to boolean cycle through vowels

--- unit_32/RealEstate.java
+++ unit_32/RealEstate.java
@@ -1,6 +1,6 @@
 //RealEstate.java
 /**
-*Thats a string and takes out all the vowels
+*Asks for a sentence to take out vowels for. 
 *
 *@author Jonathan MacLeod
 *@version Last_Modified: Nov 22nd, 2014
@@ -10,22 +10,44 @@ import java.util.*;
 
 class RealEstate
 {
-	public static void main (String[]args)
+	public static void main (String[]args) //grabs input, sends out string to get fixed
 	{
 
 		System.out.println(" This programs will remove all your vowels ");
 
 		Scanner keyboard = new Scanner (System.in);
 		System.out.println("Enter your ad sentence");
-		String adLine = keyboard.nextLine();
+		String adline = keyboard.nextLine();	//grabs sentence to fix
 
-		String fixedLine = lineFixer(adLine);
-
-		System.out.println("Your sentence backwards is " + fixedLine);
+		String lineFixed = lineFixer(adline);	//send to lineFixer method
+		System.out.println ("\r" + lineFixed);	//print result
 	}
 
-	String lineFixer(String line)
+	static String lineFixer(String line) //takes out vowels, returns fixed string
 	{
+		String lineOutput = "";			//will be the final vowelless output
 
+		for (int z = 0; z < line.length(); z++)
+		{
+
+			if (!notVowel(line.charAt(z)))  
+			{
+				char a = line.charAt(z);      //get the char at the # char we looking at
+				lineOutput += line.charAt(z);  //update the new string, not including vowel
+			}
+		}
+		return lineOutput;
 	}
+
+	static boolean notVowel (char c)  //check out the char, is it a vowel listed? 
+									  //if so, return true
+	{
+		String vowels = "aeiou";   //the vowels not to use
+     for (int i=0; i<5; i++)
+     {
+       if (c == vowels.charAt(i))
+         return true;
+     }
+     return false;
+   }
 }
